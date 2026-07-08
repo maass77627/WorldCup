7ef54f0bbc717d5e3556c7e0a4e8db83b1e43095
Kelly Maass <kmaass77627@gmail.com>
2026-07-08 12:50:07 -0500

    setting up add team form

--- src/AddTeamForm.js
+++ src/AddTeamForm.js
@@ -1,16 +1,34 @@
 
-
+import { useState } from "react";
 
 
 
 function AddTeamForm() {
+const [formData, setFormData] = useState({
+    name: "",
+    group_name: "",
+    flag_code: "",
+    fifa_rank: ""
 
+})
+
+function handleChange(e) {
+    setFormData((prev) => ({...prev, [e.target.name]: e.target.value}))
+}
+
+function handleSubmit(e) {
+    e.preventDefault()
+    console.log("submitting form")
+}
 
 
     return (
-        <form>
-            <input type="text"></input>
-           <button>Add Team</button>
+        <form onSubmit={(e) => handleSubmit(e)}>
+            <input onChange={(e) => handleChange(e)} name="name" type="text" value={formData.name}></input>
+            <input onChange={(e) => handleChange(e)} name = "group_name" type="text" value={formData.group_name}></input>
+            <input onChange={(e) => handleChange(e)} name="flag_code" type="text" value={formData.flag_code}></input>
+            <input onChange={(e) => handleChange(e)} name="fifa_rank" type="text"  value={formData.fifa_rank}></input>
+           <button type="submit">Add Team</button>
         </form>
     )
 }
