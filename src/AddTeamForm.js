01c7811f8f1968407cf131f777abc6658ab9915a
Kelly Maass <kmaass77627@gmail.com>
2026-07-08 11:57:29 -0500

    Create add team form component

---
+++ src/AddTeamForm.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+function AddTeamForm() {
+
+
+
+    return (
+        <form>
+            <input type="text"></input>
+           <button>Add Team</button>
+        </form>
+    )
+}
+
+export default AddTeamForm
