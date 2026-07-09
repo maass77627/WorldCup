e394245e57d3b31d362f0164c9a8e92fe081e1cc
Kelly Maass <kmaass77627@gmail.com>
2026-07-09 12:43:29 -0500

    add matches table

--- src/MatchHeader.js
+++ src/MatchHeader.js
@@ -6,8 +6,10 @@ function MatchHeader({matches}) {
 
     return (
         <header className="match-header">
+            <div className="match-text">
          <h1>All Matches</h1>
          <p>{matches.length} Matches</p>
+         </div>
          <button className="add-match-button">+ Add Match</button>
         </header>
     )
